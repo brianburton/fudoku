35dc20a3124f992f8117b891a701df57b925a538
Brian Burton <brian@burton-computer.com>
2021-08-04 17:35:25 -0400

    Refactoring

--- FudokuTests/UnitTest1.fs
+++ FudokuTests/UnitTest1.fs
@@ -7,6 +7,8 @@ open Puzzle
 let unsolvedCell r c ds =
     unsolvedCell (position r c) (Set.ofList ds)
 
+let addCell cell map = Map.add cell.position cell map
+
 [<SetUp>]
 let Setup () = ()
 
@@ -16,48 +18,44 @@ let singlePencilTests () =
     let p19 = position One Nine
 
     let before =
-        addToPuzzle
-            emptyPuzzle
-            [ (unsolvedCell One One [ Two ])
-              (unsolvedCell One Nine [ Eight ]) ]
+        emptyPuzzle
+        |> addCell (unsolvedCell One One [ Two ])
+        |> addCell (unsolvedCell One Nine [ Eight ])
 
     let expected = [ p11, Solved Two; p19, Solved Eight ]
 
     let actual = SingleDigit.rule (cellFinder before)
 
     Assert.AreEqual(expected, actual.changes)
-    Assert.Pass()
 
 [<Test>]
 let hiddenPairTest () =
     let group = row (One)
 
-    let combo : DigitCombination =
+    let combo: DigitCombination =
         { inside = [ One; Two ]
-          outside =
-              [ Three
-                Four
-                Five
-                Six
-                Seven
-                Eight
-                Nine ] }
+          outside = List.except [ One; Two ] AllDigits }
+
+    let outsideDigits = List.except [ Three; Nine ] AllDigits
 
     let before =
-        addToPuzzle
-            emptyPuzzle
-            [ (unsolvedCell One Three [ One;Two;Four;Five; Six; Seven;Eight ])
-              (unsolvedCell One Four [ One;Two;Four;Five; Six; Seven;Eight ])
-              (unsolvedCell One Five [ One;Two;Four;Five; Six; Seven;Eight ])
-              (unsolvedCell One Six [ One;Two;Four;Five; Six; Seven;Eight ])
-              (unsolvedCell One Seven [ One;Two;Four;Five; Six; Seven;Eight ])
-              (unsolvedCell One Eight [ One;Two;Four;Five; Six; Seven;Eight ])
-              (unsolvedCell One Nine [ One;Two;Four;Five; Six; Seven;Eight ]) ]
+        emptyPuzzle
+        |> addCell (unsolvedCell One Three outsideDigits)
+        |> addCell (unsolvedCell One Four outsideDigits)
+        |> addCell (unsolvedCell One Five outsideDigits)
+        |> addCell (unsolvedCell One Six outsideDigits)
+        |> addCell (unsolvedCell One Seven outsideDigits)
+        |> addCell (unsolvedCell One Eight outsideDigits)
+        |> addCell (unsolvedCell One Nine outsideDigits)
 
-    let retained = RetainPencils (Set.ofList [Three;Nine])
-    let expected = [ (position One One),retained ; (position One Two),retained ]
+    let retained =
+        RetainPencils(Set.ofList [ Three; Nine ])
 
-    let actual = Tuple.hiddenPencils group combo (cellFinder before)
+    let expected =
+        [ (position One One), retained
+          (position One Two), retained ]
+
+    let actual =
+        Tuple.hiddenPencils group combo (cellFinder before)
+
     Assert.AreEqual(expected, actual.changes)
-
-    Assert.Pass()
